3c5e3a3a8e5b264cf450ebaf789bbf9d9af4be12
Jan Andrzejewski <90138353+Jonaszekk@users.noreply.github.com>
2022-08-16 09:55:54 +0200

    Update info.php

--- info.php
+++ info.php
@@ -25,7 +25,7 @@ include_once 'header.php';
         <div class="inner-container">
             <h1>O mnie</h1>
             <p class="text">
-Witaj, jestem... Janek, jestem graczem i postanowilem stworzyć blog o grach dla mnie uwielbionych czyli o produkcjach firmy Blizzard. W WoW'e moim głównym charem jest Marksmanship Hunter( 60lvl, 204 ilvl), w Overwatchu mainuje Reinharta i Tobjorna, a w Diablo Mnicha. Zamierzam tutaj pisać o wszystkim co związane jest z Blizzardem, gdyż sprawia mi to radość i chce się podzielić z wami nowinkami i własną opiniią na te tematy. Mam nadzieje ze przyjmiecie mnie cieplutko
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in
          </p>
             <div class="skills">
                 <span>Kontakt:</span>
