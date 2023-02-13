b739c092129248db330147eebc7dd67e87ae1c4e
Jan Andrzejewski <90138353+Jonaszekk@users.noreply.github.com>
2023-02-13 19:01:12 +0100

    Update tablica.php

--- tablica/tablica.php
+++ tablica/tablica.php
@@ -45,7 +45,10 @@ include "logic.php";
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 
 
+  <?php
+  include_once 'footer.php';
 
+  ?>
   </body>
 
   </html>
